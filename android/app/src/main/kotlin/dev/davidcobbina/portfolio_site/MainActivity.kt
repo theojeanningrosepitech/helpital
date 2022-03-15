86eb8957dfc4f97c6f930f4e92388ee3db99c53d
jeanningros <theo.jeanningros@epitech.eu>
2022-03-15 20:09:52 +0900

    new

--- android/app/src/main/kotlin/dev/davidcobbina/portfolio_site/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.helpital.portfolio_site
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/dev/davidcobbina/portfoliosite/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.helpital.portfoliosite
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
